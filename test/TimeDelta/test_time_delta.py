13504a2f133305dfcbe4e735b5ccf839cf84fdb1
MJ-DE17 <manasadevi.j@diggibyte.com>
2026-01-29 16:30:24 +0530

    finds the absolute difference in seconds

---
+++ test/TimeDelta/test_time_delta.py
@@ -0,0 +1,28 @@
+import unittest
+import sys
+import os
+
+sys.path.append(
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "../../src/TimeDelta")
+    )
+)
+
+from util import time_delta
+
+
+class TestTimeDelta(unittest.TestCase):
+
+    def test_sample_1(self):
+        t1 = "Sun 10 May 2015 13:54:36 -0700"
+        t2 = "Sun 10 May 2015 13:54:36 -0000"
+        self.assertEqual(time_delta(t1, t2), 25200)
+
+    def test_sample_2(self):
+        t1 = "Sat 02 May 2015 19:54:36 +0530"
+        t2 = "Fri 01 May 2015 13:54:36 -0000"
+        self.assertEqual(time_delta(t1, t2), 88200)
+
+
+if __name__ == "__main__":
+    unittest.main()
